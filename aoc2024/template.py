3329504f4a27dbf8e597b36c75e436684c6ae5ad
Duncan Turnbull <duncan.turnbull@gmail.com>
2024-12-02 08:58:10 +0000

    Setup for 2024
    Justfile for running the download of the puzzle and input , submitting an answer
    
    Latest template from aoc2019

---
+++ aoc2024/template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+
+# from collections import defaultdict, deque
+# import re
+# from tqdm import tqdm
+# import numpy as np
+# from dataclasses import dataclass, field
+# from functools import cache,lru_cache
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def parse(lines):
+    return lines
+
+
+if __name__ == "__main__":
+    test_infile = sys.argv[0][:-3] + "-test.txt"
+    test_data = open(test_infile).read().strip()
+    test_lines = [x for x in test_data.split("\n")]
+    test_parsed = parse(test_lines)
+
+    infile = sys.argv[0][:-3] + "-input.txt"
+    data = open(infile).read().strip()
+    lines = [x for x in data.split("\n")]
+    parsed = parse(lines)
+    p1 = part1(test_parsed)
+    print("Part 1")
+    print("======")
+    if p1 == 45:
+        print(p1)
+        print(part1(parsed))
+    else:
+        print(f"failed - {p1}")
+    p2 = part2(test_parsed)
+    print("Part 2")
+    print("======")
+    if p2 == 5:
+        print(p2)
+        print(part2(parsed))
+    else:
+        print(f"failed - {p2}")
